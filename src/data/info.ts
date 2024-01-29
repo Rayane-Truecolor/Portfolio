8275ef5815e4af4ea270d4d46bf6c08aab45f8e9
Rayane-Truecolor <hassaine.ray@gmail.com>
2024-01-29 23:48:58 +0100

    first commit

---
+++ src/data/info.ts
@@ -0,0 +1,75 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "HASSAÏNE Rayane",
+  jobDescription: "Développeur web",
+  about: `Je suis un développeur web basé en Ile-de-France.
+  BAC+3 dans le développement/conception d'application, j'ai crée divers projets qui soit fonctionnel sur Internet, qu'il s'agisse de sites Web ou d'applications.
+  J'ai 5 années d'expériences dans le développement d'application web et mobile.
+  Je suis spécialisé dans l'environnement Javascript, plus précisement MERN.
+  Je suis compétent dans d'autre framework tel que ReactJs, Vue§.js, Next.js ou encore Laravel et Symfony.
+  Forger dans le métier grâce à plusieurs experience en entreprise, je cherche mettre à projet mes compétences professionnel en intégrant une entreprise.`,
+
+  experience: [
+
+    {
+      name: "Développeur REACT Native chez 'Socializus'",
+      location: "Paris",
+      startDate: "Janvier 2023",
+      endDate: "Aout 2023",
+      description: [
+        "- Mettre à jour une application de rencontre avec React native",
+      ],
+    },
+    {
+      name: " Développeur REACT chez 'PELAVO'",
+      location: "Grenoble",
+      startDate: "Mai 2022",
+      endDate: "Juillet 2022",
+      description: [
+        "- Développer un site web via méthode MERN (mongoDB, Express, React, Node)",
+      ],
+    },
+    {
+      name: "Integrateur web chez 'La Bonne Boite'",
+      location: "Paris",
+      startDate: "Octobre 2020",
+      endDate: "Novembre 2020",
+      description: [
+        "- Maintenance de site web intranet",
+        "- Construction et Administration d'un système réseau (Windows, SQL/PHP, HTML)",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Stage developpeur web et administrateur reseau chez 'Eiffage Construction'",
+      location: "Montreuil",
+      startDate: "Avril 2021",
+      endDate: "Juillet 2021",
+      description: [
+        "- Formation Développeur fullstack ",
+      ],
+    },
+    
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Uzzii_21",
+    github: "https://github.com/Rayane-Truecolor",
+    email: "mailto:hassaine.ray@gmail.com",
+    linkedin: "https://www.linkedin.com/in/rayane-hassaine/?originalSubdomain=fr",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    },
+    
+  ],
+};
